255c794ef15d8b5145f37596059d7d0a4953b1dd
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-12-14 22:51:19 -0300

    Atualização do Curso

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 ![GitHub top language](https://img.shields.io/github/languages/top/CarlosRobertoMedeiros/Backend-Java-red)
 ## Study Rest Assured
-	- For JSON
-	- For XML
+	- Trabalhando com RestAssured Básico
+		- Junit 4
+		- Modo Fluente
+
+	- Trabalhando com Validações
+		Links: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+			   http://hamcrest.org/JavaHamcrest/tutorial
+		- Usando o Hamcrest
+		- Validando o Body
+		- Json de Primeiro Nível
+		- Json de Segundo Nível
+		- Json com Lista
+		- Mensagem de Erro
+		- Lista na Raiz
+		- Verificações Avançadas
+		- Unindo jsonpath com Java
+
+	- Trabalhando com XML
+		- Nó raiz
+		- XML Avançado
+		- Unindo XmlPath com Java
+		Link xpath: 
+			https://www.red-gate.com/simple-talk/development/dotnet-development/xpath-css-dom-and-selenium-the-rosetta-stone/
+			OIhar a parte referente a XPath 	 
+		
+		- XPath
+		
+	- Organização
+		- Atributos Estáticos
+		- Request / Response Specification
+		
+	- Verbos Http Rest
+		Link: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+		- Post
+		- Validação ao Salvar
 	
--- src/main/java/br/com/roberto/rest/UserXMLTest.java
+++ src/main/java/br/com/roberto/rest/UserXMLTest.java
@@ -6,16 +6,75 @@ import static org.hamcrest.Matchers.*;
 import java.util.ArrayList;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.internal.path.xml.NodeImpl;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 public class UserXMLTest {
 
+	public static RequestSpecification reqSpec;
+	public static ResponseSpecification respSpec;
+	
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "https://restapi.wcaquino.me";
+//		RestAssured.port = 443;
+//		RestAssured.basePath = "/v2"; //Normalmente altera as versões
+		
+		RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+		reqBuilder.log(LogDetail.ALL);
+		reqSpec = reqBuilder.build();
+		
+		ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+		responseSpecBuilder.expectStatusCode(200);
+		respSpec = responseSpecBuilder.build();
+		
+		//Colocando aqui eu herdo automaticamente
+		RestAssured.requestSpecification= reqSpec;
+		RestAssured.responseSpecification= respSpec;
+				
+	}
+	
+	@Test
+	public void devoTrabalharComBaseUri() {
+
+		given()
+			//.spec(reqSpec)
+		.when()
+			.get("/usersXml/3")
+		.then()
+			//.statusCode(200)
+			//.spec(respSpec)
+
+			.rootPath("user")
+			.body("name", is("Ana Julia"))
+			.body("@id", is("3")) // Para XML tudo é String
+
+			.rootPath("user.filhos")
+			.body("name.size()", is(2))
+
+			.detachRootPath("filhos")
+			.body("filhos.name[0]", is("Zezinho"))
+			.body("filhos.name[1]", is("Luizinho"))
+			.appendRootPath("filhos")
+			.body("name", hasItem("Zezinho"))
+			.body("name", hasItems("Luizinho", "Zezinho"));
+			;
+	}
+	
 	@Test
 	public void devoTrabalharComXML() {
+		
 
-		given().when().get("https://restapi.wcaquino.me/usersXML/3").then().statusCode(200)
+		given().when().get("/usersXML/3").then().statusCode(200)
 
 				.rootPath("user").body("name", is("Ana Julia")).body("@id", is("3")) // Para XML tudo é String
 
@@ -29,7 +88,7 @@ public class UserXMLTest {
 
 	@Test
 	public void devoFazerPesquisasAvancadasComXML() {
-		given().when().get("https://restapi.wcaquino.me/usersXML").then().statusCode(200)
+		given().when().get("/usersXML").then().statusCode(200)
 				.body("users.user.size()", is(3)).body("users.user.findAll{it.age.toInteger()<=25}.size()", is(2))
 				.body("users.user.@id", hasItems("1", "2", "3"))
 				.body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
@@ -43,7 +102,7 @@ public class UserXMLTest {
 
 	@Test
 	public void devoFazerPesquisasAvancadasComXMLEJava() {
-		ArrayList<NodeImpl> nomes = given().when().get("https://restapi.wcaquino.me/usersXML").then().statusCode(200)
+		ArrayList<NodeImpl> nomes = given().when().get("/usersXML").then().statusCode(200)
 				.extract().path("users.user.name.findAll{it.toString().contains('n')}");
 
 		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
@@ -52,15 +111,29 @@ public class UserXMLTest {
 	
 	@Test
 	public void devoFazerPesquisasAvancadasComXPath() {
-		
+		//Roseta Stone XPath vai mostrar um documento com várias características de busca com XPath
 		given()
 			.when()
-				.get("https://restapi.wcaquino.me/usersXML")
+				.get("/usersXML")
 			.then()
 				.statusCode(200)
 				.body(hasXPath("count(/users/user)", is("3")))
+				.body(hasXPath("/users/user[@id = '1']"))
+				.body(hasXPath("//user[@id = '1']"))
+				.body(hasXPath("//name[text() = 'Luizinho']/../../name", is("Ana Julia")))
+				.body(hasXPath("//name[text() = 'Ana Julia']/following-sibling::filhos", allOf(containsString("Zezinho"),containsString("Luizinho"))))
+				.body(hasXPath("/users/user/name", is("João da Silva")))
+				.body(hasXPath("//name", is("João da Silva")))
+				.body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+				.body(hasXPath("/users/user[last()]/name", is("Ana Julia")))
+				.body(hasXPath("count(/users/user/name[contains(.,'n')])", is("2")))
+				.body(hasXPath("//user[age < 24]/name", is("Ana Julia")))
+				.body(hasXPath("//user[age < 24]/name", is("Ana Julia")))
+				.body(hasXPath("//user[age > 20 and age < 30]/name", is("Maria Joaquina")))
+				.body(hasXPath("//user[age > 20][age < 30]/name", is("Maria Joaquina")))
 			;
 	}
 //https://www.cod3r.com.br/courses/take/rest-assured/lessons/9520488-xpath
 //Continuar a partir dos 4 minutos
 }
+
